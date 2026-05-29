86c842e17156374b92b9fdce7fa970d8ca8162cd
ethan.kim <ethan.kim@beyondedge.co>
2026-05-29 15:31:30 +0700

    Migrate project to Bedrock structure

--- app/public/wp-content/themes/stafflink/front-page.php
+++
@@ -1,8 +0,0 @@
-<?php
-get_header(); ?>
-
-<div class="container">
-	<h1>This is the home page</h1>
-</div>
-
-<?php get_footer(); ?>
--- app/public/wp-content/themes/stafflink/template-parts/breadcrumb.php
+++
@@ -1,5 +0,0 @@
-<?php
-if ( function_exists('yoast_breadcrumb') ) {
-	yoast_breadcrumb( '<div class="custom-yoast-breadcrumb">', '</div>' );
-}
-?>
